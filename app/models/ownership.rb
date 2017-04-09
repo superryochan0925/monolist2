cf4d8e2b78eb43a7516fd18f65011b8b8abc3ffb
ryo takeda <r.takeda0925@gmail.com>
2017-04-09 12:51:25 +0000

    ranking

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,8 @@
 class Ownership < ActiveRecord::Base
   belongs_to :user, class_name: "User"
   belongs_to :item, class_name: "Item"
+  
+  def self.ranking_ownerships
+    self.group(:item_id).order('count_all desc').limit(10).count
+  end
 end
